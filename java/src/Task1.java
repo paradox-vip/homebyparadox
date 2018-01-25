89be7cf36ad332cb6bd2f65072eb71d97bbed425
paradox-vip <35777110+paradox-vip@users.noreply.github.com>
2018-01-25 21:23:37 +0200

    Paradox test3

--- java/src/Task1.java
+++ java/src/Task1.java
@@ -2,5 +2,6 @@ public class Task1 {
     public static void main(String[] args) {
         System.out.println("Are still here?");
         System.out.println("Are still here?");
+        System.out.println("Are still here2?");
     }
 }

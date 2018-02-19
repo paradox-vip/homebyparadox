09236050e7fc87ef040a3bcf678ad56cb2b3f9ad
Paradox <paradoxxx.vip@gmail.com>
2018-02-19 21:38:09 +0200

    lesson6

--- java/src/ua/paradox/hometask1/ua/paradox/ooop/Program.java
+++ java/src/ua/paradox/hometask1/ua/paradox/ooop/Program.java
@@ -2,9 +2,12 @@ package ua.paradox.hometask1.ua.paradox.ooop;
 
 public class Program {
     public static void main(String[] args) {
-        Cat cat=new Cat();
-
-        cat.move();
-        cat.voice();
+        Cat cst = new Cat();
+        Dog dog = new Dog();
+        Bird bird = new Bird();
+        Pet[]pets=new Pet[]{cst,bird};
+        for (Pet myLovePet: pets){
+            myLovePet.move();
+        }
     }
 }
